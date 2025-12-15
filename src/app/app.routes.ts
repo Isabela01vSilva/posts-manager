d4ff7e1a5d69f1daf7af330e1086d67af3fce123
Isabela01vSilva <isabela.vsilva@bandtec.com.br>
2025-12-15 20:01:26 -0300

    refactor: layout and routing components for improved structure and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListPostsPageComponent } from './features/posts/pages/list-posts-page/list-posts-page.component';
 import { PostDetailsPageComponent } from './features/posts/pages/post-details-page/post-details-page.component';
+import { CreatePostComponent } from './features/posts/pages/create-post/create-post.component';
 
 export const routes: Routes = [
-  { path: '', component: ListPostsPageComponent },
   { path: 'posts', component: ListPostsPageComponent },
-  { path: 'posts/:id', component: PostDetailsPageComponent },
+  { path: 'post/edit', component: CreatePostComponent },
+  { path: 'post/:id', component: PostDetailsPageComponent },
 ];
