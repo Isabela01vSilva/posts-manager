95f401f1414a5adccf0c77a37151fe2e79a773f0
Isabela01vSilva <isabela.vsilva@bandtec.com.br>
2025-12-17 16:05:33 -0300

    feat: add comment form component

---
+++ src/app/features/posts/pages/post-details-page/components/comment-form/comment-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-form',
+  imports: [],
+  templateUrl: './comment-form.component.html'
+})
+export class CommentFormComponent {
+
+}
