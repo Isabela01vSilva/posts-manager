0ca26128c9a438cb4a9e938f440151b9ef4c8ac3
Isabela01vSilva <isabela.vsilva@bandtec.com.br>
2025-12-17 15:41:05 -0300

    feat: implement list posts page with search, sorting, and pagination features

--- src/app/features/posts/pages/list-posts-page/list-posts-page.component.ts
+++ src/app/features/posts/pages/list-posts-page/list-posts-page.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { IPost } from '../../../../core/models/post.model';
+import { ApiService } from '../../../../core/services/api-service';
+import { CommonModule } from '@angular/common';
+import { PostCardComponent } from './components/post-card/post-card.component';
+import { OrderButtonComponent } from './components/order-button/order-button.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-list-posts-page',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [
+    CommonModule,
+    PostCardComponent,
+    OrderButtonComponent,
+    PaginationComponent,
+    SearchInputComponent,
+  ],
   templateUrl: './list-posts-page.component.html',
-  styleUrl: './list-posts-page.component.css'
 })
-export class ListPostsPageComponent {
+export class ListPostsPageComponent implements OnInit {
+  allPosts: IPost[] = [];
+  filteredPosts: IPost[] = [];
+  displayedPosts: IPost[] = [];
 
+  page = 1;
+  postsPerPage = environment.postsPerPage;
+
+  sortOrder: 'asc' | null = null;
+  searchQuery = '';
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.api.get<IPost[]>('posts').subscribe((posts) => {
+      this.allPosts = posts;
+      this.applyFilters();
+    });
+  }
+
+  onSortChange(order: 'asc' | null) {
+    this.sortOrder = order;
+    this.applyFilters();
+  }
+
+  onSearch(query: string) {
+    this.searchQuery = query;
+    this.page = 1;
+    this.applyFilters();
+  }
+
+  onPageChange(newPage: number) {
+    this.page = newPage;
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    this.filteredPosts = [...this.allPosts];
+    if (this.searchQuery) {
+      this.filteredPosts = this.filteredPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+    if (this.sortOrder === 'asc') {
+      this.filteredPosts.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    this.updateDisplayedPosts();
+  }
+
+  updateDisplayedPosts() {
+    const start = (this.page - 1) * this.postsPerPage;
+    const end = start + this.postsPerPage;
+    this.displayedPosts = this.filteredPosts.slice(start, end);
+  }
 }
