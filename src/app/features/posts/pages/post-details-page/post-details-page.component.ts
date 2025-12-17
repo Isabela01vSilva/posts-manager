2bfe630d61e0989a4ea3a1790fa200cae2879fb9
Isabela01vSilva <isabela.vsilva@bandtec.com.br>
2025-12-17 16:06:26 -0300

    feat: implement post details page with edit, delete, and comment functionalities

--- src/app/features/posts/pages/post-details-page/post-details-page.component.ts
+++ src/app/features/posts/pages/post-details-page/post-details-page.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { PostCommentComponent } from './components/post-comment/post-comment.component';
+import { CommentFormComponent } from './components/comment-form/comment-form.component';
+import { IconButtonComponent } from '../../../../shared/components/icon-button/icon-button.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IPost } from '../../../../core/models/post.model';
+import { ApiService } from '../../../../core/services/api-service';
 
 @Component({
   selector: 'app-post-details-page',
-  imports: [],
+  imports: [PostCommentComponent, CommentFormComponent, IconButtonComponent],
   templateUrl: './post-details-page.component.html',
-  styleUrl: './post-details-page.component.css'
 })
 export class PostDetailsPageComponent {
+  post!: IPost;
 
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.paramMap.get('id');
+
+    if (postId) {
+      this.api.get<IPost>(`posts/${postId}`).subscribe((data) => {
+        this.post = data;
+      });
+    }
+  }
+
+  editPost() {
+    this.router.navigate([`/post/edit/${this.post.id}`]);
+  }
+
+  deletePost() {}
+
+  goBackToPosts() {
+    this.router.navigate(['/posts']);
+  }
 }
