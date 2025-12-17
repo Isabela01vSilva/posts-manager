abbed3037085b87ad0c19646fba8845b39536e54
Isabela01vSilva <isabela.vsilva@bandtec.com.br>
2025-12-17 14:47:54 -0300

    feat: add routing and posts API service with RxJS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListPostsPageComponent } from './features/posts/pages/list-posts-page/list-posts-page.component';
 import { PostDetailsPageComponent } from './features/posts/pages/post-details-page/post-details-page.component';
-import { CreatePostComponent } from './features/posts/pages/create-post/create-post.component';
+import { EditPostComponent } from './features/posts/pages/edit-post/edit-post.component';
 
 export const routes: Routes = [
   { path: 'posts', component: ListPostsPageComponent },
-  { path: 'post/edit', component: CreatePostComponent },
-  { path: 'post/:id', component: PostDetailsPageComponent },
+  { path: 'post/edit/:id', component: EditPostComponent },
+  { path: 'post/details/:id', component: PostDetailsPageComponent },
 ];
